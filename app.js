b9d2f3296f4e81c223eb3935ee800b03d5f5b2d2
omkarkawatgi26 <omkawatgi@gmail.com>
2023-09-14 23:20:34 +0530

    Minor Changes

--- app.js
+++ app.js
@@ -37,13 +37,15 @@ db.once("open", () => {
     console.log("Database Connected")
 })
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(__dirname + "/public"));
+
 
 
 
